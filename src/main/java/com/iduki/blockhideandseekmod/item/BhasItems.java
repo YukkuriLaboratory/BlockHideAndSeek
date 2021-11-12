482f41772416e16a15f7e27f0cf32211d2667143
turtton <watagame2579@gmail.com>
2021-11-12 18:11:17 +0900

    :hammer: add BhasItems class call method

--- src/main/java/com/iduki/blockhideandseekmod/item/BhasItems.java
+++ src/main/java/com/iduki/blockhideandseekmod/item/BhasItems.java
@@ -12,4 +12,10 @@ public class BhasItems {
     private static <T extends Item> T register(String id, T item) {
         return Registry.register(Registry.ITEM, new Identifier(BlockHideAndSeekMod.MOD_ID, id), item);
     }
+
+    /**
+     * このクラス自体をロードして上のアイテム達を登録させてやるためだけに生まれた虚空の戦士．
+     */
+    public static void init() {
+    }
 }
