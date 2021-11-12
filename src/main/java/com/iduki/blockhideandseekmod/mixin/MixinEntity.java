a16224b017e212c55cc073421c1ed579d69055ef
turtton <watagame2579@gmail.com>
2021-11-12 22:16:23 +0900

    :hammer: create hiding system

---
+++ src/main/java/com/iduki/blockhideandseekmod/mixin/MixinEntity.java
@@ -0,0 +1,23 @@
+package com.iduki.blockhideandseekmod.mixin;
+
+import com.iduki.blockhideandseekmod.game.HideController;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Entity.class)
+public class MixinEntity {
+    @Inject(
+            method = "setSneaking",
+            at = @At("HEAD")
+    )
+    private void setPlayerHidingState(boolean sneaking, CallbackInfo ci) {
+        var entity = ((Entity) (Object) this);
+        if (entity instanceof ServerPlayerEntity player) {
+            HideController.tryHide(player);
+        }
+    }
+}
