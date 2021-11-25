5e39c5963579e0f79920d599c7d4eb39edd1e3d7
turtton <watagame2579@gmail.com>
2021-11-25 21:47:04 +0900

    :hammer: add filter hideable block

--- src/main/java/com/iduki/blockhideandseekmod/item/ItemBlockSelector.java
+++ src/main/java/com/iduki/blockhideandseekmod/item/ItemBlockSelector.java
@@ -1,18 +1,24 @@
 package com.iduki.blockhideandseekmod.item;
 
 import com.iduki.blockhideandseekmod.game.HideController;
+import com.iduki.blockhideandseekmod.game.HudDisplay;
+import net.minecraft.block.SlabBlock;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.Items;
 import net.minecraft.network.packet.s2c.play.CooldownUpdateS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 
 import java.util.List;
 
 public class ItemBlockSelector extends LoreItem implements ServerSideItem {
+
+    private static final String SELECT_BLOCK_ERROR = "selectBlockError";
 
     private static final Settings SETTINGS = new Settings();
 
@@ -47,7 +53,12 @@ public class ItemBlockSelector extends LoreItem implements ServerSideItem {
                 return ActionResult.PASS;
             }
             var block = context.getWorld().getBlockState(context.getBlockPos());
-            HideController.updateSelectedBlock(player, block);
+            if (block.getMaterial().isSolid() && !(block.getBlock() instanceof SlabBlock)) {
+                HideController.updateSelectedBlock(player, block);
+            } else {
+                var text = new LiteralText("このブロックには擬態できません!").setStyle(Style.EMPTY.withColor(Formatting.RED));
+                HudDisplay.setActionBarText(player.getUuid(), SELECT_BLOCK_ERROR, text, 30L);
+            }
             //連打防止用
             var coolTime = 10;
             player.getItemCooldownManager().set(this, coolTime);
