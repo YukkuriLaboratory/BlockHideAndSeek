0b5046a4d687f38fd078448dd372dc1745dd872d
turtton <watagame2579@gmail.com>
2021-11-26 14:55:54 +0900

    :hammer: disable spawn protection

--- src/main/java/com/iduki/blockhideandseekmod/mixin/MixinMinecraftDedicatedServer.java
+++ src/main/java/com/iduki/blockhideandseekmod/mixin/MixinMinecraftDedicatedServer.java
@@ -1,9 +1,14 @@
 package com.iduki.blockhideandseekmod.mixin;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.dedicated.MinecraftDedicatedServer;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftDedicatedServer.class)
 public class MixinMinecraftDedicatedServer {
@@ -16,5 +21,14 @@ public class MixinMinecraftDedicatedServer {
     )
     private void allowFlight(MinecraftDedicatedServer instance, boolean b) {
         instance.setFlightEnabled(true);
+    }
+
+    @Inject(
+            method = "isSpawnProtected",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void disableSpawnProtection(ServerWorld world, BlockPos pos, PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
     }
 }
