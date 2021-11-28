1a7d18d60d5b6f12c97449ad8c3f4c7e64245cf6
turtton <watagame2579@gmail.com>
2021-11-28 12:26:40 +0900

    :hammer: fix stop command

--- src/main/java/com/iduki/blockhideandseekmod/command/Stop.java
+++ src/main/java/com/iduki/blockhideandseekmod/command/Stop.java
@@ -2,10 +2,16 @@ package com.iduki.blockhideandseekmod.command;
 
 import com.iduki.blockhideandseekmod.BlockHideAndSeekMod;
 import com.iduki.blockhideandseekmod.game.GameStart;
+import com.iduki.blockhideandseekmod.game.GameState;
 import com.iduki.blockhideandseekmod.game.PreparationTime;
+import com.iduki.blockhideandseekmod.game.TeamSelector;
 import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.network.MessageType;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+
+import java.util.UUID;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
@@ -24,8 +30,27 @@ public class Stop {
     }
 
     public static int stopGame(CommandContext<ServerCommandSource> context) {
-        PreparationTime.stopGame();
-        GameStart.stopGame();
+        var source = context.getSource();
+        switch (GameState.getCurrentState()) {
+            case IDLE -> source.sendError(Text.of("[BHAS] ゲームが開始されていません"));
+            case SELECT_TEAM -> {
+                TeamSelector.suspend();
+                broadCastSuspendMessage();
+            }
+            case PREPARE -> {
+                PreparationTime.stopGame();
+                broadCastSuspendMessage();
+            }
+            case RUNNING -> {
+                GameStart.stopGame();
+                broadCastSuspendMessage();
+            }
+        }
         return 1;
     }
+
+    private static void broadCastSuspendMessage() {
+        BlockHideAndSeekMod.SERVER.getPlayerManager().broadcastChatMessage(Text.of("[BHAS] ゲームが中断されました"), MessageType.CHAT, UUID.randomUUID());
+    }
+
 }
