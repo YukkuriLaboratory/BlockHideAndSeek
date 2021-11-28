8d9b92aceeb6d02a53c23b8d8d6ecad46ae5b966
turtton <watagame2579@gmail.com>
2021-11-28 15:32:33 +0900

    :hammer: fix logger mod id

--- src/main/java/com/iduki/blockhideandseekmod/BlockHideAndSeekMod.java
+++ src/main/java/com/iduki/blockhideandseekmod/BlockHideAndSeekMod.java
@@ -15,7 +15,7 @@ public class BlockHideAndSeekMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final String MOD_ID = "blockhideandseekmod";
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	public static MinecraftServer SERVER;
 
