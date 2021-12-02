53eacb2fff6bbbce96d1166aa996d55832e782bf
turtton <watagame2579@gmail.com>
2021-12-02 16:29:32 +0900

    :hammer: create team item sets

--- src/main/java/com/iduki/blockhideandseekmod/item/BhasItems.java
+++ src/main/java/com/iduki/blockhideandseekmod/item/BhasItems.java
@@ -5,18 +5,26 @@ import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import java.util.Set;
+
 public class BhasItems {
+  // 共通
+  public static ItemFlyer FLYER = register("flyer", new ItemFlyer());
 
-    public static ItemFlyer FLYER = register("flyer", new ItemFlyer());
-
-    public static ItemDetector DETECTOR = register("detector", new ItemDetector());
+  // 鬼用
+  public static ItemDetector DETECTOR = register("detector", new ItemDetector());
     public static ItemScanner SCANNER = register("scanner", new ItemScanner());
 
-    public static ItemBlockSelector SELECTOR = register("selector", new ItemBlockSelector());
+  // ミミック用
+  public static ItemBlockSelector SELECTOR = register("selector", new ItemBlockSelector());
 
     private static <T extends Item> T register(String id, T item) {
         return Registry.register(Registry.ITEM, new Identifier(BlockHideAndSeekMod.MOD_ID, id), item);
     }
+
+  public static final Set<Item> seekerItems = Set.of(DETECTOR, SCANNER, FLYER);
+
+  public static final Set<Item> hiderItems = Set.of(SELECTOR, FLYER);
 
     /**
      * このクラス自体をロードして上のアイテム達を登録させてやるためだけに生まれた虚空の戦士．
