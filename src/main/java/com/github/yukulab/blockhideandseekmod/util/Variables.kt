f1055e06b0fc2fbea9d31cb8e4fe92759ff14673
turtton <watagame2579@gmail.com>
2021-12-08 17:14:59 +0900

    :hammer: add global server field getter

---
+++ src/main/java/com/github/yukulab/blockhideandseekmod/util/Variables.kt
@@ -0,0 +1,7 @@
+package com.github.yukulab.blockhideandseekmod.util
+
+import com.github.yukulab.blockhideandseekmod.BlockHideAndSeekMod
+import net.minecraft.server.MinecraftServer
+
+internal val server: MinecraftServer
+    get() = BlockHideAndSeekMod.SERVER
