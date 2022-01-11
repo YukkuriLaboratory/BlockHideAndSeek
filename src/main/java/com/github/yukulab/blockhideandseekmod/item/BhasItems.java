5dd8b7345452085aff6e406c9154d9c85725643d
iduki <idukichannel@gmail.com>
2022-01-11 13:14:06 +0900

    :hammer: add Jammer

--- src/main/java/com/github/yukulab/blockhideandseekmod/item/BhasItems.java
+++ src/main/java/com/github/yukulab/blockhideandseekmod/item/BhasItems.java
@@ -21,6 +21,7 @@ public class BhasItems {
     // ミミック用
     public static ItemBlockSelector SELECTOR = register("selector", new ItemBlockSelector());
     public static ItemBlink BLINK = register("brink", new ItemBlink());
+    public static ItemJammer JAMMER = register("jammer", new ItemJammer());
 
     private static <T extends Item> T register(String id, T item) {
         return Registry.register(Registry.ITEM, new Identifier(BlockHideAndSeekMod.MOD_ID, id), item);
@@ -28,7 +29,7 @@ public class BhasItems {
 
     public static final List<LoreItem> seekerItems = List.of(DETECTOR, SCANNER, BLOCK_VIEWER, FLYER);
 
-    public static final List<LoreItem> hiderItems = List.of(SELECTOR, BLINK, FLYER);
+    public static final List<LoreItem> hiderItems = List.of(SELECTOR, BLINK, JAMMER, FLYER);
 
     public static boolean isModItem(Item item) {
         return Objects.equals(Registry.ITEM.getId(item).getNamespace(), BlockHideAndSeekMod.MOD_ID);
