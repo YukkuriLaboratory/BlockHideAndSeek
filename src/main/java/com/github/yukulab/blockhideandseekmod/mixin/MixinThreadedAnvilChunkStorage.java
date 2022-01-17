d05773a28fd7ceeb0591e54580c6da3e71bc0632
turtton <watagame2579@gmail.com>
2022-01-17 21:54:13 +0900

    :hammer: prevent resend player data and remove fake block

---
+++ src/main/java/com/github/yukulab/blockhideandseekmod/mixin/MixinThreadedAnvilChunkStorage.java
@@ -0,0 +1,32 @@
+package com.github.yukulab.blockhideandseekmod.mixin;
+
+import com.github.yukulab.blockhideandseekmod.util.HideController;
+import net.minecraft.entity.Entity;
+import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ThreadedAnvilChunkStorage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ThreadedAnvilChunkStorage.class)
+public class MixinThreadedAnvilChunkStorage {
+    /**
+     * チャンクが再読み込みされるタイミングで擬態ブロックの情報を再送信します.
+     */
+    @Redirect(
+            method = "sendChunkDataPackets",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/server/world/ThreadedAnvilChunkStorage$EntityTracker;entity:Lnet/minecraft/entity/Entity;"
+            )
+    )
+    private Entity sendHidingBlock(ThreadedAnvilChunkStorage.EntityTracker instance, ServerPlayerEntity player) {
+        var entity = instance.entity;
+        if (entity instanceof ServerPlayerEntity mimic && HideController.isHiding(mimic)) {
+            var hidingPos = HideController.getHidingPlayerMaps().get(mimic.getUuid());
+            player.networkHandler.sendPacket(new BlockUpdateS2CPacket(hidingPos, HideController.getHidingBlock(hidingPos)));
+        }
+        return entity;
+    }
+}
