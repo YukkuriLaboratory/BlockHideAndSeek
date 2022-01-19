a510f2b096968a28a7ef134614449081d19b4bab
turtton <watagame2579@gmail.com>
2022-01-19 22:30:33 +0900

    :hammer: return shulker spawn packet

--- src/main/java/com/github/yukulab/blockhideandseekmod/entity/BlockHighlightEntity.kt
+++ src/main/java/com/github/yukulab/blockhideandseekmod/entity/BlockHighlightEntity.kt
@@ -3,10 +3,14 @@ package com.github.yukulab.blockhideandseekmod.entity
 import com.github.yukulab.blockhideandseekmod.util.TeamCreateAndDelete
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.mob.ShulkerEntity
+import net.minecraft.network.Packet
+import net.minecraft.network.packet.s2c.play.MobSpawnS2CPacket
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.util.registry.Registry
 import net.minecraft.world.World
 
-class BlockHighlightEntity(type: EntityType<ShulkerEntity>, world: World) : ShulkerEntity(type, world), ModifiedTracker {
+class BlockHighlightEntity(type: EntityType<ShulkerEntity>, world: World) : ShulkerEntity(type, world),
+    ModifiedTracker {
 
     init {
         isInvisible = true
@@ -23,4 +27,7 @@ class BlockHighlightEntity(type: EntityType<ShulkerEntity>, world: World) : Shul
 
     override fun initGoals() {}
 
+    override fun createSpawnPacket(): Packet<*> = MobSpawnS2CPacket(this).apply {
+        entityTypeId = Registry.ENTITY_TYPE.getRawId(EntityType.SHULKER)
+    }
 }
