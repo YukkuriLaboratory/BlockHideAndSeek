36566a99d5f4243181706b5ea7d6d29ff2b12a50
turtton <watagame@vivaldi.net>
2022-05-21 22:01:27 +0900

    :hammer: 埋まり防止機能の追加

--- src/main/java/com/github/yukulab/blockhideandseekmod/util/PlayerUtil.kt
+++ src/main/java/com/github/yukulab/blockhideandseekmod/util/PlayerUtil.kt
@@ -2,6 +2,7 @@ package com.github.yukulab.blockhideandseekmod.util
 
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.server.network.ServerPlayerEntity
 
 object PlayerUtil {
     @JvmStatic
@@ -12,6 +13,23 @@ object PlayerUtil {
                     StatusEffects.SATURATION, 50, 200, false, false, false
                 )
             )
+        }
+    }
+
+    /**
+     * プレイヤーがブロックに埋まっていた場合、上方向に脱出させます
+     */
+    @JvmStatic
+    fun escapeFromBlock(player: ServerPlayerEntity) {
+        val world = player.world
+        var target = player.blockPos
+        var currentState = world.getBlockState(target)
+        if (currentState.material.isSolid) {
+            do {
+                target = target.up(3)
+                currentState = world.getBlockState(target)
+            } while (currentState.material.isSolid && world.topY >= target.y)
+            player.refreshPositionAfterTeleport(target.x.toDouble(), target.y.toDouble(), target.z.toDouble())
         }
     }
 }
