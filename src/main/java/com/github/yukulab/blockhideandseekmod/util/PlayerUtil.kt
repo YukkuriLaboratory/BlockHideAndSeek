85e2b1b432db6c093e3fb200bc6c8a9ba28fbfa0
turtton <top.gear7509@turtton.net>
2022-05-24 22:20:30 +0900

    :bug: fix inBlock detection algorithm

--- src/main/java/com/github/yukulab/blockhideandseekmod/util/PlayerUtil.kt
+++ src/main/java/com/github/yukulab/blockhideandseekmod/util/PlayerUtil.kt
@@ -1,8 +1,14 @@
 package com.github.yukulab.blockhideandseekmod.util
 
+import net.minecraft.block.BlockState
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.util.function.BooleanBiFunction
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Box
+import net.minecraft.util.shape.VoxelShapes
+import net.minecraft.world.World
 
 object PlayerUtil {
     @JvmStatic
@@ -24,12 +30,25 @@ object PlayerUtil {
         val world = player.world
         var target = player.blockPos
         var currentState = world.getBlockState(target)
-        if (currentState.material.isSolid) {
+        val boxWidth = player.width * 0.8f
+        var hitBox = Box.of(player.eyePos, boxWidth.toDouble(), 1.0E-6, boxWidth.toDouble())
+        if (isInsideWall(hitBox, currentState, world)) {
             do {
                 target = target.up(3)
+                hitBox = hitBox.offset(0.0, target.y.toDouble(), 0.0)
                 currentState = world.getBlockState(target)
-            } while (currentState.material.isSolid && world.topY >= target.y)
+            } while (isInsideWall(hitBox, currentState, world) && world.topY >= target.y)
             player.refreshPositionAfterTeleport(target.x.toDouble(), target.y.toDouble(), target.z.toDouble())
         }
     }
+
+    private fun isInsideWall(hitBox: Box, blockState: BlockState, world: World): Boolean =
+        BlockPos.stream(hitBox).anyMatch {
+            !blockState.isAir && blockState.shouldSuffocate(world, it) && VoxelShapes.matchesAnywhere(
+                blockState.getCollisionShape(world, it)
+                    .offset(it.x.toDouble(), it.y.toDouble(), it.z.toDouble()),
+                VoxelShapes.cuboid(hitBox),
+                BooleanBiFunction.AND
+            )
+        }
 }
